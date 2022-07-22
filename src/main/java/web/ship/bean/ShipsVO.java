856f78c45c4cc11536a9f4d2d52058b2bd38b10d
Otto <s9999776@gmail.com>
2022-07-22 16:37:29 +0800

    change ShipsVO.java ShipTotalVO.java

--- src/main/java/web/ship/bean/ShipsVO.java
+++ src/main/java/web/ship/bean/ShipsVO.java
@@ -1,27 +1,31 @@
 package web.ship.bean;
 
 import java.io.Serializable;
-
-import javax.xml.crypto.Data;
+import java.sql.Date;
 
 public class ShipsVO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer shipNo;
 	private String shipName;
-	private Data starStatus;
-	private Data lastmaintenance_Time;
+	private Date starStatus;
+	private Date lastmaintenanceTime;
+	private Integer floorOfShip;
+	private Integer shipStatusNo;
 	
 	
 	public ShipsVO() {
 		super();
 	}
-	public ShipsVO(Integer shipNo, String shipName, Data starStatus, Data lastmaintenance_Time) {
+	public ShipsVO(Integer shipNo, String shipName, Date starStatus, Date lastmaintenanceTime, Integer floorOfShip,
+			Integer shipStatusNo) {
 		super();
 		this.shipNo = shipNo;
 		this.shipName = shipName;
 		this.starStatus = starStatus;
-		this.lastmaintenance_Time = lastmaintenance_Time;
+		this.lastmaintenanceTime = lastmaintenanceTime;
+		this.floorOfShip = floorOfShip;
+		this.shipStatusNo = shipStatusNo;
 	}
 	public Integer getShipNo() {
 		return shipNo;
@@ -35,17 +39,29 @@ public class ShipsVO implements Serializable{
 	public void setShipName(String shipName) {
 		this.shipName = shipName;
 	}
-	public Data getStarStatus() {
+	public Date getStarStatus() {
 		return starStatus;
 	}
-	public void setStarStatus(Data starStatus) {
+	public void setStarStatus(Date starStatus) {
 		this.starStatus = starStatus;
 	}
-	public Data getLastmaintenance_Time() {
-		return lastmaintenance_Time;
+	public Date getLastmaintenanceTime() {
+		return lastmaintenanceTime;
 	}
-	public void setLastmaintenance_Time(Data lastmaintenance_Time) {
-		this.lastmaintenance_Time = lastmaintenance_Time;
+	public void setLastmaintenanceTime(Date lastmaintenanceTime) {
+		this.lastmaintenanceTime = lastmaintenanceTime;
+	}
+	public Integer getFloorOfShip() {
+		return floorOfShip;
+	}
+	public void setFloorOfShip(Integer floorOfShip) {
+		this.floorOfShip = floorOfShip;
+	}
+	public Integer getShipStatusNo() {
+		return shipStatusNo;
+	}
+	public void setShipStatusNo(Integer shipStatusNo) {
+		this.shipStatusNo = shipStatusNo;
 	}
 	
 	
